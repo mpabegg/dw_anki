051bf494e93bd2c29b19c5cd1b38bd0758c1a455
Matheus Abegg <matheus.abegg@gmail.com>
2021-02-28 19:11:52 +0100

    run black

--- dw_anki/dw_anki.py
+++ dw_anki/dw_anki.py
@@ -7,17 +7,19 @@ import base64
 import re
 import sys
 import logging
-import subprocess #to call lame/convert to resize media
+import subprocess  # to call lame/convert to resize media
 import operator
 
-DW_URL = 'https://learngerman.dw.com/'
-DECK_NAME = 'DW Nicos Weg A1'
-IMAGES_DIR = 'images'
-AUDIO_DIR = 'audio'
+DW_URL = "https://learngerman.dw.com/"
+DECK_NAME = "DW Nicos Weg A1"
+IMAGES_DIR = "images"
+AUDIO_DIR = "audio"
 log = logging.getLogger(__name__)
+
 
 class AnkiCard:
     cardCount = 0
+
     def __init__(self, deck):
         self.deck = deck
         self.tags = []
@@ -54,11 +56,11 @@ class AnkiCard:
         log.debug("addGerman after: " + ", ".join(self.german))
 
     def getEnglish(self):
-        entries = list(set(self.english)) # remove duplicates
+        entries = list(set(self.english))  # remove duplicates
         return "<br><br>".join(entries)
 
     def getGerman(self):
-        entries = list(set(self.german)) # remove duplicates
+        entries = list(set(self.german))  # remove duplicates
         return "<br><br>".join(entries)
 
 
@@ -66,43 +68,39 @@ class AnkiCard:
 # Interacting with AnkiConnect
 #
 
+
 def request(action, **params):
-    return json.dumps({'action': action, 'params': params, 'version': 6})
+    return json.dumps({"action": action, "params": params, "version": 6})
+
 
 def invoke(requestJson):
-    #requestJson = json.dumps(request(action, **params))
-    #response = json.load(urllib2.urlopen(urllib2.Request('http://localhost:8765', requestJson)))
-    response = (requests.post('http://localhost:8765', requestJson)).json()
+    # requestJson = json.dumps(request(action, **params))
+    # response = json.load(urllib2.urlopen(urllib2.Request('http://localhost:8765', requestJson)))
+    response = (requests.post("http://localhost:8765", requestJson)).json()
     if len(response) != 2:
-        raise Exception('response has an unexpected number of fields')
-    if 'error' not in response:
-        raise Exception('response is missing required error field')
-    if 'result' not in response:
-        raise Exception('response is missing required result field')
-    if response['error'] is not None:
-        raise Warning(response['error'])
-    return response['result']
+        raise Exception("response has an unexpected number of fields")
+    if "error" not in response:
+        raise Exception("response is missing required error field")
+    if "result" not in response:
+        raise Exception("response is missing required result field")
+    if response["error"] is not None:
+        raise Warning(response["error"])
+    return response["result"]
+
 
 def storeMediaFileJSON(filename, data64):
     request = {
         "action": "storeMediaFile",
         "version": 6,
-        "params": {
-            "filename": filename,
-            "data": data64
-        }
+        "params": {"filename": filename, "data": data64},
     }
     return json.dumps(request)
 
+
 def createDeckJSON(deck):
-    request = {
-        "action": "createDeck",
-        "version": 6,
-        "params": {
-            "deck": deck
-        }
-    }
+    request = {"action": "createDeck", "version": 6, "params": {"deck": deck}}
     return json.dumps(request)
+
 
 def addNoteJSON(deck, tags, front, back):
     request = {
@@ -112,16 +110,11 @@ def addNoteJSON(deck, tags, front, back):
             "note": {
                 "deckName": deck,
                 "modelName": "Basic",
-                "fields": {
-                    "Front": front,
-                    "Back": back
-                },
-                "options": {
-                    "allowDuplicate": False
-                },
-                "tags": tags
+                "fields": {"Front": front, "Back": back},
+                "options": {"allowDuplicate": False},
+                "tags": tags,
             }
-        }
+        },
     }
     return json.dumps(request)
 
@@ -130,28 +123,29 @@ def addNoteJSON(deck, tags, front, back):
 # Parsing data from HTML
 #
 
+
 def getGermanFromRow(reihe):
     try:
         woerter = reihe.xpath('.//strong[@dir="auto"]/text()')
-        notizen = reihe.xpath('.//div[1]/div/p/text()')
-        notiz = (''.join(notizen)).replace('\n','')
-        #//*[@id="html_body"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/p/text()
+        notizen = reihe.xpath(".//div[1]/div/p/text()")
+        notiz = ("".join(notizen)).replace("\n", "")
+        # //*[@id="html_body"]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/p/text()
         wort = woerter[0]
         if notiz:
             wort = wort + " <br><small><i>" + notiz + "</i></small>"
         return wort
     except:
-        log.warning('Failed to find any word on the german side.')
+        log.warning("Failed to find any word on the german side.")
         return None
 
 
 def getEnglishFromRow(row):
-    word = row.xpath('.//div[3]/div/p/text()')
-    if not word: # some words randomly in a table
-        word = row.xpath('.//div[3]/div/table/tbody/tr/td/text()')
+    word = row.xpath(".//div[3]/div/p/text()")
+    if not word:  # some words randomly in a table
+        word = row.xpath(".//div[3]/div/table/tbody/tr/td/text()")
     if word:
-        return word[0] #TODO: check that we only got 1?
-    log.warning('Failed to find any word on the english side.')
+        return word[0]  # TODO: check that we only got 1?
+    log.warning("Failed to find any word on the english side.")
     return None
 
 
@@ -159,8 +153,8 @@ def getImageURLFromRow(row):
     img_url = row.xpath('.//img[@class="img-responsive"]/@src')
     if not img_url:
         return ""
-    #downloadFromURL(DW_URL + img_url[0], os.path.basename(img_url[0]))
-    return (DW_URL + img_url[0])
+    # downloadFromURL(DW_URL + img_url[0], os.path.basename(img_url[0]))
+    return DW_URL + img_url[0]
 
 
 def getAudioURLFromRow(row):
@@ -175,7 +169,7 @@ def downloadFromURL(url, path):
         return 1
     r = requests.get(url, stream=True)
     if r.status_code == 200:
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             for chunk in r:
                 f.write(chunk)
         return 1
@@ -186,38 +180,47 @@ def fileToBase64(path):
     with open(path, "rb") as fh:
         return base64.b64encode(fh.read()).decode()
 
+
 def reduceImageSize(path):
-    backupDir = "{}/{}".format(IMAGES_DIR,"backup")
+    backupDir = "{}/{}".format(IMAGES_DIR, "backup")
     if not os.path.isdir(backupDir):
         os.mkdir(backupDir)
     fileName = os.path.basename(path)
-    backupFilePath = "{}/{}".format(backupDir,fileName)
+    backupFilePath = "{}/{}".format(backupDir, fileName)
     # We'll use the backup copy to generate reduced file.
     # If it exists don't copy b/c repeat runs will shrink more
     if not os.path.isfile(backupFilePath):
         os.system("cp {} {}".format(path, backupFilePath))
-    res = subprocess.run(["convert", "-resize", "25%", backupFilePath, path],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.run(
+        ["convert", "-resize", "25%", backupFilePath, path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     if res.returncode != 0:
         log.error("Failed to reduce image size: " + path)
     return
 
+
 def reduceAudioSize(path):
-    backupDir = "{}/{}".format(AUDIO_DIR,"backup")
+    backupDir = "{}/{}".format(AUDIO_DIR, "backup")
     if not os.path.isdir(backupDir):
         os.mkdir(backupDir)
     fileName = os.path.basename(path)
-    backupFilePath = "{}/{}".format(backupDir,fileName)
+    backupFilePath = "{}/{}".format(backupDir, fileName)
     # We'll use the backup copy to generate reduced file.
     # If it exists don't copy b/c repeat runs will shrink more
     if not os.path.isfile(backupFilePath):
         os.system("cp {} {}".format(path, backupFilePath))
-    res = subprocess.run(["lame", "-b", "32", backupFilePath, path],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    res = subprocess.run(
+        ["lame", "-b", "32", backupFilePath, path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
     if res.returncode != 0:
         log.error("Failed to reduce audio size: " + path)
         log.error(res.stderr)
     return
+
 
 def getVocabRows(tree):
     rows = tree.xpath('//div[@class="row vocabulary "]')
@@ -225,10 +228,11 @@ def getVocabRows(tree):
         log.error("No rows found with vocabulary")
     return rows
 
+
 def storeImage(imgURL):
     if not imgURL:
         return None
-    imgFilename = re.sub(r'\s+', '_', os.path.basename(imgURL))
+    imgFilename = re.sub(r"\s+", "_", os.path.basename(imgURL))
     imgPath = "{}/{}".format(IMAGES_DIR, imgFilename)
     log.info("Downloading image: " + imgURL)
     dlSuccess = downloadFromURL(imgURL, imgPath)
@@ -241,10 +245,11 @@ def storeImage(imgURL):
             return imgFilename
     return None
 
+
 def storeAudio(audioURL):
     if not audioURL:
         return None
-    audioFilename = re.sub(r'\s+', '_', os.path.basename(audioURL))
+    audioFilename = re.sub(r"\s+", "_", os.path.basename(audioURL))
     audioPath = "{}/{}".format(AUDIO_DIR, audioFilename)
     log.info("Downloading audio: " + audioURL)
     dlSuccess = downloadFromURL(audioURL, audioPath)
@@ -258,9 +263,10 @@ def storeAudio(audioURL):
     log.warning("No audio available:" + de)
     return None
 
+
 def buildAnkiFromURL(cards, vocabURL):
     try:
-        lessonName = (re.search('en\/([^\/]+)\/', vocabURL)).group(1)
+        lessonName = (re.search("en\/([^\/]+)\/", vocabURL)).group(1)
     except AttributeError:
         log.critical("No lesson name in URL: " + vocabURL)
         raise SystemExit(1)
@@ -274,12 +280,14 @@ def buildAnkiFromURL(cards, vocabURL):
         de = getGermanFromRow(row)
         en = getEnglishFromRow(row)
         if en == None or de == None:
-            log.info('Could not find english and/or german word from row. Possibly the last row is empty div.')
+            log.info(
+                "Could not find english and/or german word from row. Possibly the last row is empty div."
+            )
             continue
         log.debug("de = " + de)
         log.debug("en = " + en)
 
-        log.info("Processing card for {} -> {}".format(en,de))
+        log.info("Processing card for {} -> {}".format(en, de))
         card = AnkiCard(DECK_NAME)
         card.addTag(tag)
 
@@ -298,12 +306,10 @@ def buildAnkiFromURL(cards, vocabURL):
         else:
             cards[en] = card
 
+
 def storeCards(cards):
-    for card in sorted(cards.values(), key = operator.attrgetter('cardNumber')):
-        req = addNoteJSON(card.deck,
-                          card.tags,
-                          card.getEnglish(),
-                          card.getGerman())
+    for card in sorted(cards.values(), key=operator.attrgetter("cardNumber")):
+        req = addNoteJSON(card.deck, card.tags, card.getEnglish(), card.getGerman())
         try:
             res = invoke(req)
             if card.hasImage:
@@ -327,19 +333,18 @@ def getLessonURL():
 def main():
     # Initialize directories needed, relative to CWD
     if not os.path.isdir(IMAGES_DIR):
-        os.mkdir(IMAGES_DIR);
+        os.mkdir(IMAGES_DIR)
     if not os.path.isdir(AUDIO_DIR):
-        os.mkdir(AUDIO_DIR);
+        os.mkdir(AUDIO_DIR)
 
     # Configure logging
     log = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S',
-                        handlers=[
-                            logging.FileHandler("run.log"),
-                            logging.StreamHandler()
-                        ])
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.FileHandler("run.log"), logging.StreamHandler()],
+    )
 
     log.info("Starting...")
 
@@ -348,7 +353,7 @@ def main():
 
     lessonURL = getLessonURL()
 
-    cards = {} # For duplicates we'll append the german values
+    cards = {}  # For duplicates we'll append the german values
     log.info("Building Anki cards from: " + lessonURL)
     buildAnkiFromURL(cards, lessonURL)
     log.info("Done with lesson: " + lessonURL)
@@ -358,6 +363,5 @@ def main():
     log.info("Done!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
